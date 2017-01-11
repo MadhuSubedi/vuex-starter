2c63f80d9215f12337305f5bd029de3c56fb017a
Madhu Sudhan <madhu.subedi@introcept.co>
2017-01-11 12:05:33 +0545

    -[x]  completed Todo Feature with Single Page

--- vuex-todo-app/main.js
+++ vuex-todo-app/main.js
@@ -7,33 +7,141 @@ const store = new Vuex.Store({
       { todo: 'Build something awesome', completed: false }
     ]
   },
+  actions: {
+      TaskCompleted ({ commit }, todo) {
+        commit('Task_Completed', todo)
+      },
+      DeleteTask ({ commit }, todo) {
+        commit('Delete_Task', todo)
+      },
+      AddTodo({commit}, newTodoText) {
+        commit('Add_Todo', newTodoText)
+      }
+  },
   mutations: {
-  	TaskCompleted: (state, todo) =>  {
-  		state.todos.todo = todo;
-  		state.todos.todo.completed = true;
-  	} 
+  	Task_Completed: (state, todo) =>  {
+  		this.todo = todo;
+      state.todos = state.todos.filter(function () {
+        return this.todo.todo.completed = true;
+      });
+  	},
+    Delete_Task: (state, todo) => {
+      let self = this;
+      self.todo = todo;
+      state.todos = state.todos.filter(function (todo) {
+        return self.todo.todo.todo != todo.todo;
+      });
+    },
+    Add_Todo: (state, newTodoText) => {
+      this.newTodoText = newTodoText.newTodoText;
+      state.todos.push({todo:this.newTodoText, completed:false})
+      state.newTodoText = '';
+    } 
   }
 })
 
-const app = new Vue({
-  el: '#app',
-  computed: {
-  	todos() {
-  		return store.state.todos;
-  	},
-	remaining(){
-		var self = this;
-		return store.state.todos.filter(function(todo) { 
-			return self.isCompleted(todo);
-		});
-	}
+let Home = {
+  template:
+   `<div class="well">
+      <h1>Hello This is Home Page</h1>
+    </div>`
+ }
+let CreateTodo = {
+  template: 
+  `<div class="well">
+    <h1>Create Todo </h1>
+    <form> 
+      <div class="form-group">
+        <label for="todo"> Todo Name </label>
+        <input type="text" v-model="newTodoText" class="form-control">
+      </div>
+
+      <div class="form-group text-right">
+        <button class="btn btn-primary" @click="addNewTodo">Save</button>
+      </div>
+    </form>
+  </div>`,
+  data(){
+    return{
+      newTodoText: '',
+      completed: false
+    }
   },
   methods: {
-	complete: function(todo) {
-		store.commit('TaskCompleted', todo)
-	},
-	isCompleted: function(todo) { 
-		return !todo.completed;
-	}
+    addNewTodo(){
+      var self = this;
+      let newTodoText = this.newTodoText;
+      store.dispatch('AddTodo', {
+        newTodoText
+      });
+      this.newTodoText = '';
+      this.$router.push('/todo');
+    }
   }
+}
+let Todo = {
+  template: `
+      <div class="row well"> 
+        <div class="col-md-12">    
+          <h1>Todos</h1>
+            <span v-if="remaining.length"> Remaining ( {{ remaining.length }} ) </span>
+
+          <ul class="list-group">
+            <li class="list-group-item" v-for="todo in todos">
+              <span class="badge" v-if="todo.completed">Done</span>
+              <span class="badge" v-else>Not Completed</span>
+
+              <span class="label label-danger" v-if="todo.completed" @click="deleteTask(todo)">&times;</span>
+              {{ todo.todo }}
+              <button 
+                class="label label-success"
+                v-if="!todo.completed" 
+                @click="complete(todo)"
+              >
+                check
+              </button>
+            </li>
+          </ul> 
+        </div>
+      </div>`,
+
+  computed: {
+    todos() {
+      return store.state.todos;
+    },
+    remaining(){
+      let self = this;
+      return store.state.todos.filter(function(todo) { 
+        return self.isCompleted(todo);
+      });
+     }
+  },
+  methods: {
+    complete(todo) {
+      store.dispatch('TaskCompleted', {
+        todo
+      })
+    },
+    isCompleted(todo) { 
+      return !todo.completed;
+    },
+    deleteTask(todo) {
+      store.dispatch('DeleteTask', {
+        todo
+      })
+    }
+  }
+}
+
+Vue.use(VueRouter)
+let routes = [
+  {path: '/', component: Home},
+  {path: '/create-todo', component: CreateTodo},
+  {path: '/todo', component: Todo},
+]
+const router = new VueRouter({
+  routes
 })
+new Vue({
+  router
+}).$mount('#app1')
